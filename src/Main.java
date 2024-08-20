0c5a889d4098a86536aa2cc90e3b80bf7b3f6789
som141 <163431679+som141@users.noreply.github.com>
2024-08-20 15:26:15 +0900

    dp
    dp2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+   public static int[][] mutiple(int [][] a1,int [][] a2){
+       int answer[][]=new int[a1.length][a2[0].length];
+       int i=0;
+       int j=0;
+       int k=0;
+       for(i=0;i<answer.length;i++){
+           for(j=0;j<answer[0].length;i++){
+               for (k=0;k<a2.length;k++){
+                   answer[i][j]+=a1[i][k]*a2[k][j];
+               }
+           }
+       }
+
+       return answer;
+   }
+}
