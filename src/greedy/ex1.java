b0fd03e3e729376a7bfcdf092858910b9607bab7
som141 <163431679+som141@users.noreply.github.com>
2024-09-01 15:01:57 +0900

    Update ex1.java

--- src/greedy/ex1.java
+++ src/greedy/ex1.java
@@ -1,4 +1,18 @@
 package greedy;
+class edge implements Comparable<edge>{
+//    간선 표현
+    int w;
+    int cost;
 
+    public edge(int w, int cost) {
+        this.w = w;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(edge o) {
+        return this.cost-o.cost;
+    }
+}
 public class ex1 {
 }
