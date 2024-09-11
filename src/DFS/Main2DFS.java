9bcfd7e699d82a2dbf9f364c3c9d1f668229dcf5
som141 <163431679+som141@users.noreply.github.com>
2024-09-11 17:44:31 +0900

    dfs

--- src/DFS/Main2DFS.java
+++ src/DFS/Main2DFS.java
@@ -3,74 +3,72 @@ package DFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main2 {
+public class Main2DFS {
     //시도는 좋은데 최선의 결과는 아님.. 반례가 있음..
     //제대로 dfs,bfs를 쓰자!
     static int count;
     static int[][]w;
+    static boolean[][]visit;
+    static int x[]={1,-1,0,0};
+    static int y[]={0,0,1,-1};
+    static int x1;
+    static int y1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(System.in));
         int num= Integer.parseInt(bufferedReader.readLine());
         for(int i=0;i<num;i++){
-            count=0;
             String s= bufferedReader.readLine();
             StringTokenizer stringTokenizer = new StringTokenizer(s," ");
             int num1 = Integer.parseInt(stringTokenizer.nextToken());
             int num2 = Integer.parseInt(stringTokenizer.nextToken());
             int num3 = Integer.parseInt(stringTokenizer.nextToken());
             w= new int[num1][num2];
-            Queue<node> queue= new PriorityQueue<>();
+            visit=new boolean[num1][num2];
+            x1=num1;
+            y1=num2;
             for(int j=0;j<num3;j++){
                 String s1= bufferedReader.readLine();
                 StringTokenizer stringTokenizer1 = new StringTokenizer(s1," ");
                 int n1 = Integer.parseInt(stringTokenizer1.nextToken());
                 int n2 = Integer.parseInt(stringTokenizer1.nextToken());
                 w[n1][n2]=1;
-                queue.add(new node(n1,n2));
             }
-            for(int j=0;j<num3;j++){
-                node n= queue.poll();
-                if(w[n.u][n.v]==1){
-                    count++;
+            count=0;
+            for(int j=0;j<num1;j++){
+                for(int k=0;k<num2;k++){
+                    if(w[j][k]==1&&!visit[j][k]){
+                        dfs(j,k);
+                        count++;
+                    }
                 }
-                if(n.u>0&&w[n.u-1][n.v]==1){
-                    w[n.u-1][n.v]=0;
-                }
-                if(n.u<num1-1&&w[n.u+1][n.v]==1){
-                    w[n.u+1][n.v]=0;
-                }
-                if(n.v>0&&w[n.u][n.v-1]==1){
-                    w[n.u][n.v-1]=0;
-                }
-                if(n.v<num2-1&&w[n.u][n.v+1]==1){
-                    w[n.u][n.v+1]=0;
-                }
-
-
             }
             System.out.println(count);
 
 
+
         }
 
     }
-    static class node implements Comparable<node>{
-        int u;
-        int v;
 
-        public node(int u,int v) {
-            this.u = u;
-            this.v = v;
+    private static void dfs(int j, int k) {
+        visit[j][k]=true;
+        for(int i=0;i<4;i++){
+            int nx=x[i]+j;
+            int ny=y[i]+k;
+
+            if(nx>=0&&nx<x1&&ny>=0&&ny<y1){
+                if(w[nx][ny]==1&&!visit[nx][ny]){
+                    dfs(nx,ny);
+                }
+            }
         }
 
-
-        @Override
-        public int compareTo(node o) {
-            return (this.u+this.v)-(o.u+o.v);
-        }
     }
+
 
 }
