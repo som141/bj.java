691b09a7cabcf2f56f124c103848814059d8ad2e
som141 <163431679+som141@users.noreply.github.com>
2024-09-23 17:03:13 +0900

    d

---
+++ src/cas/IncrementInteger.java
@@ -0,0 +1,7 @@
+package cas;
+
+public interface IncrementInteger {
+
+    void increment();
+    int get();
+}
---
+++ src/cas/IncrementThreadMain.java
@@ -0,0 +1,14 @@
+package cas;
+
+public class IncrementThreadMain {
+    public static final int THREAD_COUNT = 1000;
+    public static void main(String[] args) {}
+    private static void test(IncrementInteger incrementInteger){
+        new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        };
+    }
+}
