cb6c9aca453d55f0b1901fd2b441ca10c1172f11
som141 <163431679+som141@users.noreply.github.com>
2024-09-23 18:15:45 +0900

    delete

--- src/cas/IncrementThreadMain.java
+++ src/cas/IncrementThreadMain.java
@@ -1,5 +1,8 @@
 package cas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IncrementThreadMain {
     public static final int THREAD_COUNT = 1000;
     public static void main(String[] args) {}
@@ -7,8 +10,15 @@ public class IncrementThreadMain {
         new Runnable() {
             @Override
             public void run() {
-                
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                incrementInteger.increment();
             }
         };
+        List<Thread> threads = new ArrayList<>();
+
     }
 }
