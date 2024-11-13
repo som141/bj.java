e40d0eb549cdf5e1dfc72f3970074b471ab3d729
som141 <163431679+som141@users.noreply.github.com>
2024-11-13 17:27:27 +0900

    dd

--- src/kmp/ex1.java
+++ src/kmp/ex1.java
@@ -3,6 +3,8 @@ package kmp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ex1 {
     public static void main(String[] args) throws IOException {
@@ -11,11 +13,47 @@ public class ex1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         s = br.readLine();
         patten = br.readLine();
-        int [] fp =failFuntion(patten);
+        List<Integer> queue = kmpSerch(s,patten);
+        for(int i:queue){
+            System.out.print(i+1+ " ");
+        }
+
+    }
+    public static List kmpSerch(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        int [] fp=failFuntion(p);
+        int j=0;
+        for(int i=0;i<s.length();i++) {
+            while(j>0&&s.charAt(i)!=p.charAt(j)) {
+                j=fp[j-1];
+            }
+            if(s.charAt(i)==p.charAt(j)) {
+                j++;
+            }
+            if(j==p.length()) {
+                list.add(i-j+1);
+                j=fp[j-1];
+
+            }
+
+        }
+        System.out.println(list.size());
+        return list;
     }
 
     private static int[] failFuntion(String patten) {
         int []fp=new int[patten.length()];
+        int j=0;
+        for(int i=1;i<patten.length();i++) {
+            while(j>0&& patten.charAt(i)!=patten.charAt(j)) {
+                j=fp[j-1];
+            }
+            if(patten.charAt(i)==patten.charAt(j)) {
+                j++;
+            }
+            fp[i]=j;
+        }
 
+        return fp;
     }
 }
